009994ab8a454d940c73f53f6814fcae1c26c071
Marz Rover <matthew@dooley.xyz>
2020-12-07 06:36:53 -0500

    Fix testing for linux

--- Tests/SwiftcraftTests/SwiftcraftTests.swift
+++ Tests/SwiftcraftTests/SwiftcraftTests.swift
@@ -2,46 +2,11 @@ import XCTest
 import class Foundation.Bundle
 
 final class SwiftcraftTests: XCTestCase {
-    func _testExample() throws { // remove prepended _ to run enable test
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        let fooBinary = productsDirectory.appendingPathComponent("Swiftcraft")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
+    func testExample() {
+        XCTAssertTrue(true)
     }
 
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
-    }
-
-    static var allTests: [(String, () -> Void)] = [
-        // ("testExample", testExample), // uncomment enable test
+    static var allTests = [
+        ("testExample", testExample),
     ]
 }
