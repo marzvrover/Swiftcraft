a256f1195463d351c38ec0f662404beb24304e11
Marz Rover <matthew@dooley.xyz>
2020-12-09 08:29:35 -0500

    docs init

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-import SwiftcraftTests
-import SwiftcraftLibraryTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftcraftTests.allTests()
-tests += SwiftcraftLibraryTests.allTests()
-XCTMain(tests)
