f28d1bc1c75e7dafe75c8066c10ca6bd3aeddc19
marz <marzily@protonmail.com>
2022-04-29 11:30:07 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "SwiftcraftLibrary", targets: ["SwiftcraftLibrary"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftpackages/DotEnv.git", from: "1.0.0"),
+        .package(url: "https://github.com/swiftpackages/DotEnv.git", from: "3.0.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.23.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
